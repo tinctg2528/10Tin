5ef97e6f6ffc5cca3e6333f33ea8f28c1f5a6446
Tan Loc <nguyentanloc2010.ttkh@gmail.com>
2025-08-29 22:38:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const descriptionElement = document.getElementById("class-description");
+    descriptionElement.textContent = "Chào mừng đến với trang web của Lớp 10 Tin, trường THPT Chuyên Tiền Giang!";
+});
